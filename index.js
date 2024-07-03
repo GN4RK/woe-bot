9af2aba9bc7b035e6ee03f84191533397630a38e
Yoann LEONARD <yoann.leonard@gmail.com>
2024-07-03 18:58:57 +0200

    Initialising project

---
+++ index.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const path = require('path');
+const {
+    Client, Collection, Events, GatewayIntentBits, EmbedBuilder, Partials, PermissionsBitField
+} = require ('discord.js');
+
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+    ],
+});
+
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            addLog("warning", `The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+
+
+
+
+const discordToken = process.env.DISCORD_TOKEN;
+
+client.login(discordToken);
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+// Slash commands
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        addLog("error", `No command matching ${interaction.commandName} was found.`)
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        addLog("error", error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'Error', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'Error', ephemeral: true });
+        }
+    }
+});
